284a69c8fe163675e5c34ac28035d775fbb9e8a8
Davi Souza <ddas.souza@gmail.com>
2024-02-15 13:00:25 -0300

    HG-2763: makes APP_URL accessible (#4)
    * Sends 200 status if webhook failed
    
    * Fixes unintentional uninstallation
    
    * Adds env vars as public
    
    ---------
    
    Co-authored-by: Davi Souza <davi@hotglue.io>

--- next.config.js
+++ next.config.js
@@ -2,10 +2,21 @@ const { parsed: localEnv } = require("dotenv").config();
 
 const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
+const appUrl = process.env.APP_URL;
 
 module.exports = {
+  env: {
+    APP_URL: appUrl,
+  },
   webpack: (config) => {
-    const env = { API_KEY: apiKey };
+    const env = {
+      API_KEY: apiKey,
+      NEXT_PUBLIC_API_KEY: apiKey,
+      SHOPIFY_API_KEY: apiKey,
+      NEXT_PUBLIC_SHOPIFY_API_KEY: apiKey,
+      APP_URL: appUrl,
+      NEXT_PUBLIC_APP_URL: appUrl,
+    };
     config.plugins.push(new webpack.DefinePlugin(env));
 
     // Add ESM support for .mjs files in webpack 4
--- pages/index.js
+++ pages/index.js
@@ -15,22 +15,20 @@ import { ExternalMinor } from "@shopify/polaris-icons";
 const Index = () => {
   useEffect(() => {
     // Automatically redirect to the app
-    window.location = process.env["APP_URL"]
+    window.location = process.env["APP_URL"];
   }, []);
 
   return (
     <Page>
       <Layout>
         <Layout.Section>
-          <Heading>{process.env['APP_NAME']}</Heading>
+          <Heading>{process.env["APP_NAME"]}</Heading>
           <p>
-            <TextStyle>
-              {process.env['APP_DESCRIPTION']}
-            </TextStyle>
+            <TextStyle>{process.env["APP_DESCRIPTION"]}</TextStyle>
           </p>
           <Card sectioned>
             <FormLayout>
-              <TextStyle>Login to {process.env['APP_NAME']}</TextStyle>
+              <TextStyle>Login to {process.env["APP_NAME"]}</TextStyle>
               <Button
                 accessibilityLabel={`Login to ${process.env["APP_NAME"]} (opens a new window)`}
                 icon={ExternalMinor}
