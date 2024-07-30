f52c6232541174c26a198e17c5c6adeff4ae82b5
Davi Souza <ddas.souza@gmail.com>
2024-07-30 13:59:29 -0300

    HG-3245: replaces oauth.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
-require('@babel/register')({
-  presets: ['@babel/preset-env'],
-  ignore: ['node_modules']
+const fs = require("fs");
+
+fs.copyFileSync(
+  "./server/hotglueOauth.js",
+  "./node_modules/@shopify/shopify-api/dist/auth/oauth/oauth.js"
+);
+
+console.log("shopify-api oauth.js updated");
+
+require("@babel/register")({
+  presets: ["@babel/preset-env"],
+  ignore: ["node_modules"],
 });
 
 // Import the rest of our application.
-module.exports = require('./server.js');
+module.exports = require("./server.js");
