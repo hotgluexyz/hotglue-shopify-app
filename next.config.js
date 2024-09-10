ba0664daddf9f41e2b0997ad0ffbb07fd562d124
Davi Souza <ddas.souza@gmail.com>
2024-09-10 13:58:33 -0300

    HG-3432: fixes redirect without tenant id

--- next.config.js
+++ next.config.js
@@ -3,10 +3,18 @@ const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 const appUrl = process.env.APP_URL;
+const hgApiUrl = process.env.HG_API_URL;
+const hgEnvId = process.env.HG_ENV_ID;
+const hgFlowId = process.env.HG_FLOW_ID;
+const hgFlowVersion = process.env.HG_FLOW_VERSION ?? "1";
 
 module.exports = {
   env: {
     APP_URL: appUrl,
+    HG_API_URL: hgApiUrl,
+    HG_ENV_ID: hgEnvId,
+    HG_FLOW_ID: hgFlowId,
+    HG_FLOW_VERSION: hgFlowVersion,
   },
   webpack: (config) => {
     const env = {
