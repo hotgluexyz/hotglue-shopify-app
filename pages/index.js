d637c14e91f81047016f9972e9426cae5a756664
Davi Souza <ddas.souza@gmail.com>
2024-09-11 18:38:44 -0300

    HG-3432: fixes redirect without tenant id (#8)

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,77 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   Heading,
   Page,
   Layout,
   Button,
-  Banner,
   Card,
   FormLayout,
-  TextField,
   TextStyle,
 } from "@shopify/polaris";
 import { ExternalMinor } from "@shopify/polaris-icons";
 
-const Index = () => {
+const makeGetRequest = async (url, apiKey) => {
+  const response = await fetch(url, {
+    headers: { "x-api-key": apiKey },
+  });
+
+  if (!response.ok) {
+    return { data: undefined };
+  }
+
+  return { data: await response.json() };
+};
+
+async function getTenantsByPrefix(hgApiUrl, hgEnvId, hgApiKey, shopId) {
+  const url = new URL(`${hgApiUrl}/tenants/${hgEnvId}`);
+
+  if (shopId) {
+    url.searchParams.set("tenant", shopId);
+  }
+
+  const resp = await makeGetRequest(url.toString(), hgApiKey);
+
+  return resp.data;
+}
+
+export async function getServerSideProps(ctx) {
+  const {
+    HG_API_URL: hgApiUrl,
+    HG_API_KEY: hgApiKey,
+    HG_ENV_ID: hgEnvId,
+  } = process.env;
+  const { shop } = ctx.query;
+  let store = undefined;
+  let tenantId = undefined;
+
+  if (shop) {
+    store = shop.split(".")[0];
+    const tenants = await getTenantsByPrefix(
+      hgApiUrl,
+      hgEnvId,
+      hgApiKey,
+      store
+    );
+    tenantId = tenants?.[0];
+  }
+
+  return { props: { store, tenantId } };
+}
+
+const Index = (props) => {
+  const { store, tenantId } = props;
+
   useEffect(() => {
-    const currentUrl = new URL(window.location);
     const url = new URL(process.env["APP_URL"]);
 
-    if (currentUrl.searchParams.has("shop")) {
-      const shopId = currentUrl.searchParams.get("shop").split(".")[0];
-      url.searchParams.set("store", shopId);
+    if (store) {
+      url.searchParams.set("store", store);
     }
 
-    // Automatically redirect to the app
+    if (tenantId) {
+      url.searchParams.set("tenantId", tenantId);
+    }
+
     window.location = url.toString();
   }, []);
 
